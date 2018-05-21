0b7effc60a3397cef8623dbe56e17f8983d68be9
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2018-05-21 23:44:06 +0200

    Added correct defaults again

--- config/Config.go
+++ config/Config.go
@@ -37,9 +37,9 @@ func (cfg *Config) FromFlags() {
 	flag.StringVar(&cfg.Db.Type, "dbtype", "stub", "Database type")
 	flag.StringVar(&cfg.Db.Url, "dburl", "localhost:5432", "Database Url")
 
-	flag.StringVar(&cfg.Mqtt.Url, "mqtturl", "tcp://localhost:1883", "Mqtt url")
-	flag.StringVar(&cfg.Mqtt.User, "mqttuser", "", "Mqtt user")
-	flag.StringVar(&cfg.Mqtt.Password, "mqttpassword", "", "Mqtt password")
+	flag.StringVar(&cfg.Mqtt.Url, "mqtturl", "tcp://192.168.1.181:1883", "Mqtt url")
+	flag.StringVar(&cfg.Mqtt.User, "mqttuser", "openhab", "Mqtt user")
+	flag.StringVar(&cfg.Mqtt.Password, "mqttpassword", "cafe123456", "Mqtt password")
 
 	flag.IntVar(&cfg.Server.Port, "serverport", 8080, "Server port")
 
